24aa0cf968b631186c32d367c2e604b1a35a82ac
Tomek Cejner <tomek.cejner@gmail.com>
2022-08-30 08:22:25 +0200

    Initial commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,13 @@
+class BooksController < ApplicationController
+  
+  def index
+  end
+  
+  def dupa
+    @pytanie = params[:q]
+  end
+  
+  def show
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monolit::Application.config.secret_token = 'c908ef97a00d70d15ba33f00eecc4173c9694ab2cc6157f48de98881b83afb101987061ab61ca9bdcffb3befe483cb20377d6272e87deabbe7d0fcb56c9a3155'
